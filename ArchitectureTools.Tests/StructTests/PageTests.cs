32b03d89901bc99f4dd51609f691d4caee58a935
Gustavo de Araujo Oliveira <gustavo.dearaujo26@gmail.com>
2024-06-08 11:15:35 -0300

    Creating pagination value object

---
+++ ArchitectureTools.Tests/StructTests/PageTests.cs
@@ -0,0 +1,87 @@
+﻿using ArchitectureTools.Pagination;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArchitectureTools.Tests.StructTests
+{
+    public class PageTests
+    {
+        [Fact]
+        public void ShouldReturnSuccess_WhenSimplePagination()
+        {
+            int totalItems = 20;
+
+            var page = new Page(1, 10, totalItems);
+
+            Assert.Equal(2, page.LastPage);
+            Assert.Equal(1, page.NextPages.Count);
+            Assert.Empty(page.PreviousPages);
+            Assert.Null(page.Previous);
+        }
+
+        [Fact]
+        public void ShouldReturnSuccess_WhenComplexPagination()
+        {
+            int totalItems = 28;
+
+            var page = new Page(1, 10, totalItems);
+
+            Assert.Equal(3, page.LastPage);
+            Assert.Equal(2, page.NextPages.Count);
+            Assert.Empty(page.PreviousPages);
+            Assert.Null(page.Previous);
+        }
+
+        [Fact]
+        public void ShouldReturnSuccess_WhenNextPage()
+        {
+            int totalItems = 28;
+
+            var page = new Page(2, 10, totalItems);
+
+            Assert.Equal(3, page.LastPage);
+            Assert.Equal(1, page.NextPages.Count);
+            Assert.NotEmpty(page.PreviousPages);
+            Assert.NotNull(page.Previous);
+        }
+
+        [Fact]
+        public void ShouldReturnSuccess_WhenLastPage()
+        {
+            int totalItems = 28;
+
+            var page = new Page(3, 10, totalItems);
+
+            Assert.NotNull(page.LastPage);
+            Assert.Equal(2, page.PreviousPages.Count);
+            Assert.NotEmpty(page.PreviousPages);
+            Assert.NotNull(page.Previous);
+        }
+
+        [Theory]
+        [InlineData(1, 10, 105678)]
+        [InlineData(50, 10, 105678)]
+        public void ShouldReturnSuccess_WhenLargeItems(int pageSelected, int pageSize, int totalItems)
+        {
+            var page = new Page(pageSelected, pageSize, totalItems);
+
+            Assert.NotNull(page.LastPage);
+            Assert.NotEmpty(page.NextPages);
+        }
+
+        [Fact]
+        public void ShouldReturnSuccess_WhenEmptyItemCount()
+        {
+            var page = new Page(1, 10);
+
+            Assert.Null(page.LastPage);
+            Assert.Null(page.Next);
+            Assert.Null(page.Previous);
+            Assert.Empty(page.PreviousPages);
+            Assert.Empty(page.NextPages);
+        }
+    }
+}
---
+++ ArchitectureTools/Pagination/Page.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections.Generic;
+
+namespace ArchitectureTools.Pagination
+{
+    /// <summary>
+    /// Página (utilizada nas paginações de listas)
+    /// </summary>
+    public struct Page
+    {
+        /// <summary>
+        /// Cria nova página
+        /// </summary>
+        /// <param name="selected">Página selecionada</param>
+        /// <param name="size">Tamanho da página</param>
+        /// <param name="itemCount">Quantidade de itens a serem paginados</param>
+        public Page(int selected, int size, int? itemCount = null)
+        {
+            Selected = selected;
+            Size = size;
+            LastPage = null;
+
+            if (itemCount.HasValue)
+                LastPage = CalculateLastPage(itemCount.Value, size);
+        }
+
+        /// <summary>
+        /// Página selecionada
+        /// </summary>
+        public int Selected { get; private set; }
+
+        /// <summary>
+        /// Tamanho da página
+        /// </summary>
+        public int Size { get; private set; }
+
+        /// <summary>
+        /// Última página
+        /// </summary>
+        public int? LastPage { get; private set; }
+
+        /// <summary>
+        /// Quantidade de itens a serem ignorados no cálculo da lista
+        /// </summary>
+        public int Skip
+        {
+            get
+            {
+                if (Selected == 1)
+                    return 0;
+
+                return (Selected - 1) * Size;
+            }
+        }
+
+        /// <summary>
+        /// Página anterior
+        /// </summary>
+        public int? Previous
+        {
+            get
+            {
+                if (Selected == 0 || Selected == 1)
+                    return null;
+
+                return Selected - 1;
+            }
+        }
+
+        /// <summary>
+        /// Lista de páginas anteriores
+        /// </summary>
+        public List<int> PreviousPages
+        {
+            get
+            {
+                if (Selected == 0 || Selected == 1)
+                    return new List<int>();
+
+                if (Previous == null || Previous == 0)
+                    return new List<int>();
+
+                var result = new List<int>();
+
+                for (int i = Previous.Value; i > 0; i--)
+                {
+                    if (result.Count == 5)
+                        break;
+
+                    result.Add(i);
+                }
+
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Próxima página
+        /// </summary>
+        public int? Next
+        {
+            get
+            {
+                if (LastPage is null)
+                    return null;
+
+                if (Selected == LastPage.Value)
+                    return null;
+
+                var next = Selected + 1;
+                if (next > LastPage.Value)
+                    return null;
+
+                return next;
+            }
+        }
+
+        /// <summary>
+        /// Lista de próximas páginas
+        /// </summary>
+        public List<int> NextPages
+        {
+            get
+            {
+                if (LastPage == null)
+                    return new List<int>();
+
+                if (Selected == LastPage.Value)
+                    return new List<int>();
+
+                if (Next == null || Next == 0)
+                    return new List<int>();
+
+                var result = new List<int>();
+
+                for (int i = Next.Value; i <= LastPage.Value; i++)
+                {
+                    if (result.Count == 5)
+                        break;
+
+                    result.Add(i);
+                }
+
+                return result;
+            }
+        }
+
+        private int? CalculateLastPage(int listCount, int pageSize)
+        {
+            if (listCount == 0 || pageSize == 0)
+                return null;
+
+            int totalPages = listCount / pageSize;
+            int remainder = listCount % pageSize;
+            if (remainder > 0)
+                totalPages += 1;
+
+            return totalPages;
+        }
+    }
+}
